adf279925f5633adc084d75ac06378b6823ce447
albertosuarezgutierrez-gif <alberto.suarez.gutierrez@gmail.com>
2026-05-06 16:15:11 +0200

    fix: GA4 ultra-minimal route.ts v3

--- app/route.ts
+++ app/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 const U = 'https://wswbehlcuxqxyinousql.supabase.co';
 const K = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzd2JlaGxjdXhxeXlpbm91c3EsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNDU2OTIsImV4cCI6MjA5MTgyMTY5Mn0.pskPJ1U-i0Vjg_suxMfXNqHOtKJpWchf0-CzLUQIzRo';
-const GA = "<!-- Google tag (gtag.js) -->\n<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-N5CMQL9C4M\"></script>\n<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-N5CMQL9C4M');</script>\n";
 export async function GET() {
   const r = await fetch(
     `${U}/rest/v1/_deploy_assets?key=like.html_chunk_*&select=key,value&order=key`,
@@ -14,7 +13,8 @@ export async function GET() {
   const ds = new DecompressionStream('gzip');
   const w = ds.writable.getWriter();
   w.write(c); w.close();
-  const html = (await new Response(ds.readable).text()).replace('</head>', GA + '</head>');
+  const html = (await new Response(ds.readable).text())
+    .replace('</head>','<script async src="https://www.googletagmanager.com/gtag/js?id=G-N5CMQL9C4M"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag("js",new Date());gtag("config","G-N5CMQL9C4M");</script></head>');
   return new NextResponse(html,{
     headers:{
       'Content-Type':'text/html; charset=utf-8',
