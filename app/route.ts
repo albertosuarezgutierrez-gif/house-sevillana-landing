facf0582d295ce7b946c293e95ae6364f36dbc5d
albertosuarezgutierrez-gif <alberto.suarez.gutierrez@gmail.com>
2026-05-27 22:36:03 +0200

    feat: Meta Pixel ID real 12124662686780882173

--- app/route.ts
+++ app/route.ts
@@ -269,7 +269,7 @@ footer{background:var(--night);color:rgba(255,255,255,.65);padding:4rem 2.5rem 2
 <!-- Meta Pixel retargeting -->
 <script>
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init','XXXXXXXXXXXXXXX');fbq('track','PageView');
+fbq('init','12124662686780882173');fbq('track','PageView');
 </script>
 </head>
 <body>
